357ad346351a7ccb6cf5cc3d224a81f72cfa70f6
ysh1005 <ysh1005@naver.com>
2021-05-13 15:16:13 +0900

    modify delete function done

--- pybo/models.py
+++ pybo/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 
 class Question(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
     subject = models.CharField(max_length=200)
     content = models.TextField()
     create_date = models.DateTimeField()
@@ -14,6 +15,7 @@ class Question(models.Model):
 
 class Answer(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+    modify_date = models.DateTimeField(null=True, blank=True)
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
     content = models.TextField()
     create_date = models.DateTimeField()
